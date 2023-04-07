38a54c3822835cbe82ef4c9d4b53b0f9e49a0de7
telit-deploy <Appz.Telit@telit.com>
2023-04-07 12:19:08 +0200

    Releasing version 1.4

---
+++ doc/html/az__defines_8h.js
@@ -0,0 +1,5 @@
+var az__defines_8h =
+[
+    [ "AZX", "az__defines_8h.html#a9a069ad45075204d798b8d1242bdffe3", null ],
+    [ "AZX_SSL", "az__defines_8h.html#a8e02e2ca6d3ef9ee2f775a9e91187881", null ]
+];
