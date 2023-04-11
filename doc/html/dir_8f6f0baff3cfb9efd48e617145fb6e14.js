fee29357bce737ff6d2a616f655bc7821ca9fffb
telit-deploy <Appz.Telit@telit.com>
2023-04-11 10:45:16 +0200

    Releasing version 1.5

---
+++ doc/html/dir_8f6f0baff3cfb9efd48e617145fb6e14.js
@@ -0,0 +1,5 @@
+var dir_8f6f0baff3cfb9efd48e617145fb6e14 =
+[
+    [ "az_defines.h", "az__defines_8h.html", "az__defines_8h" ],
+    [ "smtp.h", "smtp_8h.html", "smtp_8h" ]
+];
---
+++ doc/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['deprecated_20list_1594',['Deprecated List',['../deprecated.html',1,'']]]
+];
